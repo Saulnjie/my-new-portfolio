a626f6fd30e1ee7da70032771e71314eaeb819d4
SAUL NJIE <SAULUS92@gmail.com>
2022-02-04 13:49:09 +0100

    deleted unused components

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,19 +1,14 @@
 import styled from "styled-components";
 
 const StyledHeader = styled.div`
-width: 80%;
-height: 100px;
-flex: 0 0 auto;
-background: blue;
-margin: auto;
-margin-top: var(--size-xl);
+  width: 80%;
+  height: 100px;
+  flex: 0 0 auto;
+  background: blue;
+  margin: auto;
+  margin-top: var(--size-xl);
 `;
 
-
 export default function Header() {
-  return (
-    <StyledHeader>
-      Text goes here....
-    </StyledHeader>
-  )
+  return <StyledHeader>Text goes here....</StyledHeader>;
 }
