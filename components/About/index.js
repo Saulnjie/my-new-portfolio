63b85da06b088cfd75e229a8334b33f99deb8365
SAUL NJIE <SAULUS92@gmail.com>
2022-02-14 09:13:48 +0100

    idk

--- components/About/index.js
+++ components/About/index.js
@@ -64,7 +64,7 @@ export default function About() {
       </StyledAboutSection>
       <StyledIconSection>
         <a>
-          <i className="FaGithub" />
+          <FaGithub />
         </a>
         <a>
           <i className="" />
