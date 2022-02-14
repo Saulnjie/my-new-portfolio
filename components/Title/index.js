539b11b11f113b4a682fda5e03e187f143997335
SAUL NJIE <SAULUS92@gmail.com>
2022-02-14 20:49:49 +0100

    Only mobil left

--- components/Title/index.js
+++ components/Title/index.js
@@ -3,7 +3,7 @@ const H2 = styled.h2`
   font-family: "Raleway";
   font-size: var(--font-size-lg);
   color: var(--color-white);
-  margin-top: 50px;
+  /* margin-top: 50px; are we seaing the project tag still on all screens? */
 `;
 
 export default H2;
